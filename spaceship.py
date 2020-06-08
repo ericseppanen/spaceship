20c5464b29759d9a72f891897d0616591e8e7dc2
Eric Seppanen <eds@reric.net>
2020-06-08 13:42:27 -0700

    add spaceship, disable fist

--- spaceship.py
+++ spaceship.py
@@ -1,4 +1,5 @@
-import os, pygame
+import os
+import pygame
 from pygame.locals import *
 from pygame.compat import geterror
 
@@ -42,46 +43,17 @@ def load_sound(name):
         raise SystemExit(str(geterror()))
     return sound
 
-
-# classes for our game objects
-class Fist(pygame.sprite.Sprite):
-    """moves a clenched fist on the screen, following the mouse"""
-
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)  # call Sprite initializer
-        self.image, self.rect = load_image("fist.bmp", -1)
-        self.punching = 0
-
-    def update(self):
-        """move the fist based on the mouse position"""
-        pos = pygame.mouse.get_pos()
-        self.rect.midtop = pos
-        if self.punching:
-            self.rect.move_ip(5, 10)
-
-    def punch(self, target):
-        """returns true if the fist collides with the target"""
-        if not self.punching:
-            self.punching = 1
-            hitbox = self.rect.inflate(-5, -5)
-            return hitbox.colliderect(target.rect)
-
-    def unpunch(self):
-        """called to pull the fist back"""
-        self.punching = 0
-
-
-class Chimp(pygame.sprite.Sprite):
+class Spaceship(pygame.sprite.Sprite):
     """moves a monkey critter across the screen. it can spin the
        monkey when it is punched."""
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)  # call Sprite intializer
-        self.image, self.rect = load_image("chimp.bmp", -1)
+        self.image, self.rect = load_image("spaceship1.png", -1)
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
         self.rect.topleft = 10, 10
-        self.move = 9
+        self.move_distance = 9
         self.dizzy = 0
 
     def update(self):
@@ -93,13 +65,11 @@ class Chimp(pygame.sprite.Sprite):
 
     def _walk(self):
         """move the monkey across the screen, and turn at the ends"""
-        newpos = self.rect.move((self.move, 0))
+        newpos = self.rect.move((self.move_distance, 0))
         if not self.area.contains(newpos):
-            if self.rect.left < self.area.left or self.rect.right > self.area.right:
-                self.move = -self.move
-                newpos = self.rect.move((self.move, 0))
-                self.image = pygame.transform.flip(self.image, 1, 0)
-            self.rect = newpos
+            self.move_distance = -self.move_distance
+            newpos = self.rect.move((self.move_distance, 0))
+        self.rect = newpos
 
     def _spin(self):
         """spin the monkey image"""
@@ -138,7 +108,7 @@ def main():
     # Put Text On The Background, Centered
     if pygame.font:
         font = pygame.font.Font(None, 36)
-        text = font.render("Pummel The Chimp, And Win $$$", 1, (10, 10, 10))
+        text = font.render("Hello World", 1, (10, 10, 10))
         textpos = text.get_rect(centerx=background.get_width() / 2)
         background.blit(text, textpos)
 
@@ -150,9 +120,8 @@ def main():
     clock = pygame.time.Clock()
     whiff_sound = load_sound("whiff.wav")
     punch_sound = load_sound("punch.wav")
-    chimp = Chimp()
-    fist = Fist()
-    allsprites = pygame.sprite.RenderPlain((fist, chimp))
+    spaceship = Spaceship()
+    allsprites = pygame.sprite.RenderPlain((spaceship))
 
     # Main Loop
     going = True
@@ -166,13 +135,15 @@ def main():
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 going = False
             elif event.type == MOUSEBUTTONDOWN:
-                if fist.punch(chimp):
-                    punch_sound.play()  # punch
-                    chimp.punched()
-                else:
-                    whiff_sound.play()  # miss
+                pass
+                #if fist.punch(spaceship):
+                #    punch_sound.play()  # punch
+                #    spaceship.punched()
+                #else:
+                #    whiff_sound.play()  # miss
             elif event.type == MOUSEBUTTONUP:
-                fist.unpunch()
+                pass
+                #fist.unpunch()
 
         allsprites.update()
 
