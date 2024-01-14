6b3560cef9771b61fcae46b227df5ec3baba6772
Eric Seppanen <eds@reric.net>
2024-01-13 22:54:53 -0800

    allow window resize
    This will scale the inner viewport to the biggest size that will fit in
    the physical window. It's a bit blurry, but is better than requiring a
    fixed pixel size.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
-use bevy::window::PresentMode;
+use bevy::render::camera::{ScalingMode, Viewport};
+use bevy::window::{PresentMode, WindowResized, WindowResolution};
 
 use crate::background::BgPlugin;
 use crate::collide::CollisionPlugin;
@@ -29,16 +30,17 @@ enum GameState {
 fn main() {
     App::new()
         .insert_resource(Msaa::Off)
+        .insert_resource(ClearColor(Color::hex("010101").unwrap()))
         .add_plugins(
             DefaultPlugins
                 // default_nearest() prevents blurring of pixel art
-                .set(ImagePlugin::default_nearest())
+                .set(ImagePlugin::default_linear())
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         title: "Spaceship!".into(),
-                        resolution: (400.0, 800.0).into(),
+                        resolution: WindowResolution::new(400.0, 800.0),
                         present_mode: PresentMode::AutoNoVsync,
-                        resizable: false,
+                        resizable: true,
                         ..default()
                     }),
                     ..default()
@@ -47,6 +49,7 @@ fn main() {
         )
         .add_state::<GameState>()
         .add_systems(Startup, setup)
+        .add_systems(Update, window_resize)
         .add_plugins((
             BgPlugin,
             PlayerPlugin,
@@ -60,5 +63,40 @@ fn main() {
 }
 
 fn setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+    let mut camera = Camera2dBundle::default();
+    camera.camera.viewport = Some(Viewport {
+        physical_size: (400, 800).into(),
+        ..default()
+    });
+
+    camera.projection.scaling_mode = ScalingMode::FixedVertical(800.0);
+
+    commands.spawn(camera);
+}
+
+// If the window gets resized, we need to update the camera viewport.
+fn window_resize(mut resize_reader: EventReader<WindowResized>, mut query: Query<&mut Camera>) {
+    if let Some(event) = resize_reader.read().last() {
+        let mut camera = query.single_mut();
+        let viewport = camera.viewport.as_mut().expect("couldn't find viewport");
+
+        let mut physical_position = UVec2::ZERO;
+
+        let physical_size = if event.height > 2.0 * event.width {
+            // A very tall window is limited by the window width.
+            let width = event.width;
+            let height = event.width * 2.0;
+            physical_position.y = ((event.height - height) / 2.0) as u32;
+            (width as u32, height as u32)
+        } else {
+            // A very wide window is limited by the window height.
+            let height = event.height;
+            let width = event.height / 2.0;
+            physical_position.x = ((event.width - width) / 2.0) as u32;
+            (width as u32, height as u32)
+        };
+
+        viewport.physical_size = physical_size.into();
+        viewport.physical_position = physical_position.into();
+    }
 }
